a2648e8df45e7044c3b2b36af34134cd514bc3eb
Jackson Didat <jacksondidat@pal-nat186-34-132.itap.purdue.edu>
2017-09-09 17:33:55 -0400

    added post button

---
+++ TrashPanda/TrashPandaBrain.swift
@@ -0,0 +1,10 @@
+//
+//  TrashPandaBrain.swift
+//  TrashPanda
+//
+//  Created by Jackson Didat on 9/9/17.
+//  Copyright © 2017 jdidat. All rights reserved.
+//
+
+import Foundation
+
--- TrashPanda/ViewController.swift
+++ TrashPanda/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var asscake: UILabel!
+    
+    var toggle = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func post(_ sender: UIButton) {
+        if (toggle) {
+            asscake.text = "titcake"
+        }
+        else {
+            asscake.text = "asscake"
+        }
+        toggle = !toggle 
+    }
+    
 }
 
